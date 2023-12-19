ffbb24d45cb8f15cd419f5f1d12295ea5e3dc9ac
Rog3rSm1th <r0g3r5@protonmail.com>
2023-12-19 21:26:44 +0100

    Implement Doc struct

--- kaitai-rs/src/kaitaistruct/doc.rs
+++ kaitai-rs/src/kaitaistruct/doc.rs
@@ -1,1 +1,11 @@
+use crate::kaitaistruct::kaitai_property::KaitaiProperty;
 
+/// Doc property struct
+///
+/// Contains:
+/// 1. A description of the Kaitai struct
+pub struct Doc {
+    pub description: Option<String>,
+}
+
+impl KaitaiProperty for Doc {}
--- kaitai-rs/src/kaitaistruct/doc_ref.rs
+++ kaitai-rs/src/kaitaistruct/doc_ref.rs
@@ -4,61 +4,10 @@ use crate::kaitaistruct::kaitai_property::KaitaiProperty;
 ///
 /// Contains:
 /// 1. URL as text,
-/// 2. arbitrary string
-/// 3. or URL as text + space + arbitrary string.
+/// 2. An arbitrary string
 pub struct DocRef {
     pub url: Option<String>,
     pub arbitrary_string: Option<String>,
 }
 
 impl KaitaiProperty for DocRef {}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn create_docref_with_url() {
-        let doc_ref_url = DocRef {
-            url: Some("https://example.com/documentation".to_string()),
-            arbitrary_string: None,
-        };
-
-        assert_eq!(
-            doc_ref_url.url,
-            Some("https://example.com/documentation".to_string())
-        );
-        assert_eq!(doc_ref_url.arbitrary_string, None);
-    }
-
-    #[test]
-    fn create_docref_with_arbitrary_string() {
-        let doc_ref_arbitrary_string = DocRef {
-            url: None,
-            arbitrary_string: Some("Some arbitrary string".to_string()),
-        };
-
-        assert_eq!(doc_ref_arbitrary_string.url, None);
-        assert_eq!(
-            doc_ref_arbitrary_string.arbitrary_string,
-            Some("Some arbitrary string".to_string())
-        );
-    }
-
-    #[test]
-    fn create_docref_with_url_and_arbitrary_string() {
-        let doc_ref_url_and_arbitrary_string = DocRef {
-            url: Some("https://example.com/documentation".to_string()),
-            arbitrary_string: Some("Some arbitrary string".to_string()),
-        };
-
-        assert_eq!(
-            doc_ref_url_and_arbitrary_string.url,
-            Some("https://example.com/documentation".to_string())
-        );
-        assert_eq!(
-            doc_ref_url_and_arbitrary_string.arbitrary_string,
-            Some("Some arbitrary string".to_string())
-        );
-    }
-}
