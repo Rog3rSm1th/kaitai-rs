05d67c93609e757fd18bc1711b5d79af6ff7047a
Rog3rSm1th <r0g3r5@protonmail.com>
2024-01-13 00:11:05 +0100

    Add command-line argument for file-path in `ksy-parser`

--- ksy-parser/src/main.rs
+++ ksy-parser/src/main.rs
@@ -1,11 +1,24 @@
 use kaitai_rs::kaitaistruct::parser::parser::KsyParser;
+use std::env;
 
 fn main() {
+    // Get command-line arguments
+    let args: Vec<String> = env::args().collect();
+
+    // Check if a file path argument is provided
+    if args.len() != 2 {
+        println!("Usage: {} <file_path>", args[0]);
+        return;
+    }
+
+    // Get the file path from the command-line argument
+    let file_path = &args[1];
+
     // Create a new instance of KsyParser
     let mut parser = KsyParser::new();
 
     // Parse YAML file
-    if let Ok(_yaml_value) = parser.parse_yaml("../kaitai_struct_formats/common/riff.ksy") {
+    if let Ok(_yaml_value) = parser.parse_yaml(file_path) {
         // Print the content of the doc_instance
         parser.print_struct();
     } else {
